d711f72eee0052f517e207134dd5bd71143f0653
Jeff Goeders <jeff.goeders@gmail.com>
2022-04-14 10:20:54 -0600

    Update example to use pip
    Signed-off-by: Jeff Goeders <jeff.goeders@gmail.com>

--- Makefile
+++ Makefile
@@ -1,7 +1,4 @@
 run:
 	python3 example.py
 
-init:
-	git submodule init
-	git submodule update
 
--- example.py
+++ example.py
@@ -1,21 +1,23 @@
-import sys
-from pathlib import Path
-
-# Add the pygrader directory to the python search path
-sys.path.append(str(Path(__file__).resolve().parent / "pygrader_repo"))
-
-import pygrader
+import ygrader
 
 
 def my_callback(student_code_path, lab_name, **kw):
+    print("*** Grading", lab_name, "***\n")
+
     lab_report_path = student_code_path / "lab_report.txt"
     if lab_report_path.is_file():
         print(open(lab_report_path).read())
     else:
-        raise pygrader.CallbackFailed("Missing lab_report.txt")
+        raise ygrader.CallbackFailed("Missing lab_report.txt")
 
 
-grader = pygrader.Grader("example", "lab1", "learning_suite/grades.csv", "lab1_labreport", 10)
+grader = ygrader.Grader(
+    name="example",
+    lab_name="lab1",
+    grades_csv_path="learning_suite/grades.csv",
+    grades_col_name="lab1_labreport",
+    points=10,
+)
 grader.set_callback_fcn(my_callback)
 grader.set_submission_system_learning_suite("learning_suite/lab1_submissions.zip")
 grader.run()
